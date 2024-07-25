c3da811d2f815c5be97f35720b13788002e0ac3f
Geneticanomaly <thomaskaatranen@gmail.com>
2024-07-25 20:32:17 +0300

    Addition of task 3.16 Phonebook database, step 4. Error handling was moved to a new error handler middleware.

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.get('/api/persons', (req, res) => {
     });
 });
 
-app.get('/info', (req, res) => {
+app.get('/info', (req, res, next) => {
     const existingPeople = [];
     Person.find({})
         .then((people) => {
@@ -35,37 +35,28 @@ app.get('/info', (req, res) => {
             res.send(`<p>Phonebook has info for ${existingPeople.length} people</p>
             <p>${time}</p>`);
         })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send({error: 'something went wrong'});
-        });
+        .catch((error) => next(error));
 });
 
-app.get('/api/persons/:id', (req, res) => {
+app.get('/api/persons/:id', (req, res, next) => {
     const id = req.params.id;
     Person.findById(id)
         .then((person) => {
             res.json(person);
         })
-        .catch((error) => {
-            console.error(error);
-            return res.status(500).send({error: 'malformatted id'});
-        });
+        .catch((error) => next(error));
 });
 
-app.delete('/api/persons/:id', (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
     const id = req.params.id;
     Person.findByIdAndDelete(id)
         .then((result) => {
             res.status(204).end();
         })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send({error: 'malformatted id'});
-        });
+        .catch((error) => next(error));
 });
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body;
 
     if (!body.name || !body.number) {
@@ -83,11 +74,21 @@ app.post('/api/persons', (req, res) => {
         .then((savedPerson) => {
             res.json(savedPerson);
         })
-        .catch((error) => {
-            console.error(error);
-            res.status(500).send({error: 'something went wrong'});
-        });
+        .catch((error) => next(error));
 });
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message);
+
+    if (error.name === 'CastError') {
+        return res.status(400).send({error: 'malformatted id'});
+    } else {
+        res.status(500).end();
+    }
+    next(error);
+};
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 
