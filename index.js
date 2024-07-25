f876c0f3ef9257cc1209c34a2758de2a33a7e7b4
Geneticanomaly <thomaskaatranen@gmail.com>
2024-07-25 21:58:41 +0300

    Addition of task 3.19 Phonebook database, step 7, Backend portion.

--- index.js
+++ index.js
@@ -79,12 +79,9 @@ app.post('/api/persons', (req, res, next) => {
 });
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const person = {
-        name: req.body.name,
-        number: req.body.number,
-    };
+    const {name, number} = req.body;
 
-    Person.findByIdAndUpdate(req.params.id, person, {new: true})
+    Person.findByIdAndUpdate(req.params.id, {name, number}, {new: true, runValidators: true, context: 'query'})
         .then((updatedPerson) => {
             res.json(updatedPerson);
         })
@@ -96,6 +93,8 @@ const errorHandler = (error, req, res, next) => {
 
     if (error.name === 'CastError') {
         return res.status(400).send({error: 'malformatted id'});
+    } else if (error.name === 'ValidationError') {
+        return res.status(400).json({error: error.message});
     } else {
         res.status(500).end();
     }
